d81a6ca1ec7755d0cf1e756208b2d65ef5979bb4
ScorpIan555 <ian.daley23@gmail.com>
2020-01-29 13:18:55 -0500

    brought server definition in to endpoint code to test

--- server/src/graphql.js
+++ server/src/graphql.js
@@ -1,5 +1,24 @@
 // graphql.js
-const { server } = "./index";
+// const server = "./index";
+const { ApolloServer } = require("apollo-server-lambda");
+const { resolvers } = "./resolvers";
+const { typeDefs } = "./type-defs";
+
+export const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context
+    // cache  // ref: https://www.apollographql.com/docs/apollo-server/data/data-sources/#using-memcachedredis-as-a-cache-storage-backend
+    // server settings
+    //   playground: process.env.NODE_ENV ? 'development' ? true : false,
+    //   introspection: true,
+    //   tracing: true
+  })
+});
 
 exports.graphqlHandler = server.createHandler({
   cors: {
